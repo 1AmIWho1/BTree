31af6aeff3d8b79c267b108e0a87fd755da37a12
Petr Kladov <petr.kladov@gmail.com>
2022-11-25 23:05:03 +0100

    new input

--- main.cpp
+++ main.cpp
@@ -9,40 +9,34 @@ using namespace std;
 
 int main(){
     ifstream file;
-    file.open("test.txt");
-    int n, m;
-    file >> n >> m;
-    BTree b(m);
+    file.open("input.txt"); // has same input as in the presentation
+    int order;
+    file >> order;
+    BTree b(order);
+    char type;
     int key;
-    for(int i = 0; i < n; ++i){
-        file >> key;
-        //cout << "adding " << key << endl;
-        b.Insert(key);
-
-    }
+    do{
+        file >> type;
+        if(type == 'i'){
+            int n;
+            file >> n;
+            for(int i = 0; i < n; ++i){
+                file >> key;
+                b.Insert(key);
+            }
+        }
+        else if(type == 'd'){
+            int n;
+            file >> n;
+            for(int i = 0; i < n; ++i){
+                file >> key;
+                b.Delete(key);
+            }
+        }
+        else if(type == 'o'){
+            b.Report();
+        }
+    } while(type != 'e');
     file.close();
-    b.Report();
-    b.Delete(3);
-    b.Delete(7);
-    b.Delete(8);
-    b.Delete(6);
-    b.Delete(22);
-    b.Delete(46);
-    b.Delete(1);
-    b.Delete(17);
-    b.Delete(55);
-    b.Delete(44);
-    b.Delete(4);
-    b.Delete(45);
-    b.Delete(15);
-    b.Delete(9);
-    b.Delete(10);
-    b.Delete(67);
-    b.Delete(2);
-    b.Delete(70);
-    b.Delete(21);
-    b.Delete(5);
-    cout << endl;
-    b.Report();
     return 0;
 }
